dd196e4843facb17a259e3f67f6b60b9384f1dd7
Wilson Souto <wilsonsouto@outlook.com.br>
2024-11-15 00:38:19 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {
+      // To change the pallete tone, you can access: https://palette.tone-row.com/
+      colors: {
+        background: '#fafafa',
+        foreground: '#222222',
+        neutral: {
+          50: '#fafafa',
+          100: '#f1f1f1',
+          200: '#e0e0e0',
+          300: '#cbcbcb',
+          400: '#999999',
+          500: '#6a6a6a',
+          600: '#4c4c4c',
+          700: '#393939',
+          800: '#222222',
+          900: '#181818',
+        },
+        accent: {
+          50: '#fff7ee',
+          100: '#ffebd5',
+          200: '#ffd3a9',
+          300: '#ffb472',
+          400: '#ff8937',
+          500: '#ff6400',
+          600: '#ef4a12',
+          700: '#c53819',
+          800: '#9b301c',
+          900: '#7d2c1b',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
